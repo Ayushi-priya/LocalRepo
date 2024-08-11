07c642146af5023dd1971e09acc1101fd4c6adc1
Ayushi Priya <ayushipriya7531@gmail.com>
2024-08-11 15:25:09 +0530

    Stock Span Problem and Next Greater Element problem done

---
+++ NextGreaterEle.java
@@ -0,0 +1,78 @@
+package LocalRepo;
+
+import java.util.Stack;
+
+public class NextGreaterEle {
+    // public static void nextGreaterElement(int arr[]) {
+    //     Stack<Integer> s = new Stack<>();
+    //     int nextGreater[] = new int[arr.length];
+
+    //     for (int i = arr.length-1; i >= 0; i--) {
+    //         while (!s.isEmpty() && arr[s.peek()] < arr[i]) {
+    //             s.pop();
+    //         }
+    //         if(s.isEmpty()) {
+    //             nextGreater[i] = -1;
+    //         } else {
+    //             nextGreater[i] = arr[s.peek()];
+    //         }
+    //         s.push(i);
+    //     }
+
+    //     for (int i = 0; i < nextGreater.length; i++) {
+    //         System.out.print(nextGreater[i] + " ");
+    //     }
+    // }
+
+    public static void nextGreaterElement(int arr[]) {
+        Stack<Integer> s = new Stack<>();
+        int nextGreater[] = new int[arr.length];
+        for(int i = arr.length-1; i >= 0; i--) {
+            while(!s.empty() && arr[s.peek()] < arr[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()) {
+                nextGreater[i] = -1;
+            } else {
+                nextGreater[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+
+        for (int i = 0; i < nextGreater.length; i++) {
+            System.out.print(nextGreater[i]+ " ");
+        }
+    }
+
+    public static void stockSpan(int stocks[], int span[]) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice >= stocks[s.peek()]) {
+                s.pop();
+            }
+            if(s.isEmpty()) {
+                span[i] = i+1;
+            } else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+            s.push(i);
+        }
+
+        System.out.println();
+        for (int i = 0; i < span.length; i++) {
+            System.out.print(span[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6, 8, 0, 1 ,3};
+        nextGreaterElement(arr);
+        int stocks[] = {100, 80, 60, 70, 60, 85, 100};
+        stockSpan(stocks, new int[stocks.length]);
+    }
+}
